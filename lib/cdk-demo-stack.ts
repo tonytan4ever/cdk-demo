19b3e6ade244722c15591be340dd3f0b80eb362b
Tony Tan <tony.tan@flatiron.com>
2024-01-24 17:23:56 -0600

    Initial commit

---
+++ lib/cdk-demo-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+
+    // Define an S3 bucket
+    new s3.Bucket(this, 'MyDemoBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
+  }
+}
