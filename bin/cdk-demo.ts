a2fc6d4326ff2b8b0c02a7053bf20ce51133d6f1
abidkhan03 <abidkhan42935@gmail.com>
2024-01-31 13:09:55 +0500

    Incorporate Step Function Batch Jobs

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import { App, Aspects } from 'aws-cdk-lib';
+import { CdkDemoStack } from '../lib/stacks/cdk-demo-stack';
+import { StepBatchStack } from '../lib/stacks/step-batch-stack';
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
 
-const app = new cdk.App();
-new CdkDemoStack(app, 'CdkDemoStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new App();
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+new CdkDemoStack(app, 'CdkDemoStack');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stepBatchStack = new StepBatchStack(app, 'StepBatchStack');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+NagSuppressions.addStackSuppressions(stepBatchStack, [
+  {
+    id: "AwsSolutions-S1",
+    reason: "overkill for this small sample",
+  },
+  {
+    id: "AwsSolutions-IAM4",
+    reason: "Managed policies are sufficient for a sample of this size",
+  },
+  {
+    id: "AwsSolutions-IAM5",
+    reason: "Some dynamic wildcard permissions are required for several service actions",
+  },
+]);
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+app.synth();
